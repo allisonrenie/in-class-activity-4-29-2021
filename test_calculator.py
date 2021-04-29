1d9585a1c6f092ba67fe027931e8b8b0f0080da0
allisonrenie <allisonrenie@gmail.com>
2021-04-29 16:58:19 -0700

    testing works

---
+++ test_calculator.py
@@ -0,0 +1,20 @@
+import unittest
+import calculator
+
+class test_class(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(calculator.add(3,4), 7)
+
+    def test_subtract(self):
+        self.assertEqual(calculator.subtract(14,7), 7)
+
+    def test_multiply(self):
+        self.assertEqual(calculator.multiply(7,11), 77)
+
+    def test_divide(self):
+        self.assertEqual(calculator.divide(21,3), 7)
+        self.asserEqual(calculator.divide(7,0), 7)              #should fail
+
+    # how to run:
+    # python -m unittest test_calculator.py
